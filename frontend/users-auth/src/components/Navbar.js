b11bb602f656479d57f2654dfdcac33e694890de
GavrielJoseph <gavriel.535220049@stu.untar.ac.id>
2024-03-31 22:16:43 +0700

    benerin navbar

--- frontend/users-auth/src/components/Navbar.js
+++ frontend/users-auth/src/components/Navbar.js
@@ -13,8 +13,14 @@ function Navbar() {
 
 
   return (
-    <nav className='flex justify-around p-3 border-b border-zinc-800 items-center bg-[#1a1a1a]/90 text-zinc-300'>
-        <Link to='/'><h1 className='text-3xl'>Fitness Tracker</h1></Link>
+    <nav className='flex justify-between p-3 border-b border-zinc-800 items-center bg-[#1a1a1a]/90 text-zinc-300'>
+        <div className="flex items-center">
+            <Link to='/' className="ml-4"><h1 className='text-3xl'>Fitness Tracker</h1></Link>
+            <div className="ml-auto flex gap-6">
+                <Link to='/routine' className="ml-4">Routine</Link>
+                <Link to='/about' className="ml-4">About</Link>
+            </div>
+        </div>
         <ul className='flex gap-6'>
             { isUserSignedIn ? (
                 <>
