55bd34f627c31565e42d20d5487ea28a719da782
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2024-04-03 13:52:07 +0700

    add route

--- server/routes/main.js
+++ server/routes/main.js
@@ -18,7 +18,7 @@ async function compare(userPass, hashPass) {
 } 
 
 //Routes
-router.get('/',  (req, res) => {
+router.get('',  (req, res) => {
 
     try{
         res.render('login', {title: 'Login', showHeader: false});
@@ -29,25 +29,28 @@ router.get('/',  (req, res) => {
 
 router.post("/signup", async (req, res) => {
     try {
-        const check = await Auth.findOne({ email: req.body.email });
-        if (check) {
-            res.send("User already exists");
-        } else {
-            const token = jwt.sign({ name: req.body.name }, "abcdefghijklmnopqrstuvwxyzabcdeghijklmnopqrstuvwxyz");
+        const checkEmail = await Auth.findOne({ email: req.body.email });
+        const checkName = await Auth.findOne({ username: req.body.username });
+        if (checkEmail) {
+            return res.send('<script>alert("Email has been used"); window.location="/signup"</script>');
+        } if (checkName){
+            return res.send('<script>alert("Username has been used"); window.location="/signup"</script>.');
+        }else {
+            const token = jwt.sign({ username: req.body.username }, "abcdefghijklmnopqrstuvwxyzabcdeghijklmnopqrstuvwxyz");
             const data = {
-                name: req.body.name,
+                username: req.body.username,
                 email: req.body.email,
                 password: await hashPass(req.body.password),
-                token: token
+                token: token 
             };
             await Auth.insertMany([data]);
-            res.send('<script>alert("User Created"); window.location="/"</script>');
-            res.redirect("/login");
+            res.send('<script>alert("User Created"); window.location="/login"</script>');
         }
-    } catch {
-        res.send("Wrong details");
-    }
-});
+    } catch (error){
+        console.error("Error during signup:", error);
+        return res.status(500).send("An error occurred during signup"); 
+    } 
+}); 
 
 //login
 router.post("/login", async (req, res) => {
@@ -55,13 +58,15 @@ router.post("/login", async (req, res) => {
         const { email, password } = req.body;
 
         if (!email || !password) {
-            return res.send('<script>alert("Please enter both email and password"); window.location="/"</script>');
+            return res.send('<script>alert("Please enter both email and password"); window.location="/login"</script>');
         }
 
         const check = await Auth.findOne({ email });
         if (!check) {
-            return res.send('<script>alert("Account not found"); window.location="/"</script>');
+            return res.send('<script>alert("Account not found"); window.location="/login"</script>');
         }
+
+        const username = check.username;
 
         const passCheck = await compare(password, check.password);
         if (passCheck) {
@@ -69,7 +74,7 @@ router.post("/login", async (req, res) => {
                 maxAge: 600000,
                 httpOnly: true
             });
-            res.redirect("/routine");
+            res.redirect("/"+username+"/routine");
         } else {
             res.send('<script>alert("Wrong password"); window.location="/"</script>');
         }
@@ -78,10 +83,11 @@ router.post("/login", async (req, res) => {
     }
 });
 
-router.get('/routine', async (req, res) => {
+router.get('/:username/routine', async (req, res) => {
     try{
-
-        res.render('index', {title: 'Routines'});
+        const userName = req.params.username;
+        const user = await Auth.findOne({ username: userName });
+        res.render('index', {title: 'Routines', user: user});
     } catch (error){
         console.log(error);
     }
